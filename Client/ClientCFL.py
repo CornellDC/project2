fb39d811e37c5c9153bd951eb4f4a338e52d72b9
CornellDC <cornell.falconerlawson@dcmail.ca>
2024-12-04 14:30:27 -0500

    Added Academic Integrity statement.

---
+++ Client/ClientCFL.py
@@ -0,0 +1,12 @@
+'''
+TPRG 2131 Project 2 - ClientCFL.py
+December 4th, 2024
+Cornell Falconer-Lawson <Cornell.FalconerLawson@dcmail.ca>
+
+This program is strictly my own work. Any material
+beyond course learning materials that is taken from
+the Web or other sources is properly cited, giving
+credit to the original author(s).
+
+
+'''
